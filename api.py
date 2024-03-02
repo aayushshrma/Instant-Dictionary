e417aaee6906ff771395c3ee4fafa10a53e4cf1f
Aayush Sharma <aayushramansharma@gmail.com>
2024-03-02 09:26:26 +0100

    finalizing api

--- api.py
+++ api.py
@@ -1,4 +1,6 @@
 import justpy as jp
+import definition
+import json
 
 
 class Api:
@@ -10,7 +12,12 @@ class Api:
     def serve(cls, req):
         wp = jp.WebPage()
         word = req.query_params.get('w')
-        wp.html = word.title()
+
+        defined = definition.Definition(word).get()
+
+        response = {"word": word, "definition": defined}
+
+        wp.html = json.dumps(response)
         return wp
 
 
